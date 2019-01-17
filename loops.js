bb7073bb97c8138520d6e96531cc095aea6df750
Al3nMicL <https://twitter.com/Al3nMicL>
2019-01-17 15:36:48 -0500

    Todo 1 tests pass

---
+++ loops.js
@@ -0,0 +1,9 @@
+// TODO 1: 'I am 1 strange loop.'
+forLoop = (array) => {
+	for (let i = 0; i < 25; i++) {
+		i === 1 ? array.push(`I am 1 strange loop.`) : array.push(`I am ${i} strange loops.`)
+	}
+	return array;
+};
+
+// TODO 2: Count down while loop
